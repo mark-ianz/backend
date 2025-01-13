f5100aee2b2e59d4e73d8d50fa035323131020ad
mark-ianz <bustillomarkian23@gmail.com>
2025-01-13 20:11:32 +0800

    added try catch block for zod validation

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import express, { Request, Response } from "express";
 import "dotenv/config";
-import { UserCreate } from "./userCreate.dtos";
+import { UserCreate, UserCreateSchema } from "./userCreate.dtos";
+import { z } from "zod";
 
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.listen(PORT, () => {
@@ -17,6 +18,23 @@ app.get("/", (req, res) => {
 });
 
 app.post("/api/user", (req: Request<{}, {}, UserCreate>, res: Response) => {
-  const username = req.body.username;
-  res.json(username);
+  try {
+    const user = UserCreateSchema.parse(req.body);
+
+    res.json({ user });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      const formattedErrors = error.errors.map((err) => ({
+        path: err.path[0],
+        message: err.message,
+      }));
+
+      res.status(400).json({ errors: formattedErrors });
+      return;
+    }
+
+    res
+      .status(500)
+      .json({ message: "There was a server error. Please try again later." });
+  }
 });
