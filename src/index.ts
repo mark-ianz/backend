d5849d766f041114de406c6dfbec9f1e362ec6c9
mark-ianz <bustillomarkian23@gmail.com>
2025-01-13 18:59:05 +0800

    created a post /api/user route and express urlencoded and json middleware for parsing

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,22 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import "dotenv/config";
+import { UserCreate } from "./userCreate.dtos";
 
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(express.urlencoded());
+app.use(express.json());
+
 app.listen(PORT, () => {
-  console.log("Listening to PORT" + PORT);
+  console.log("Listening to PORT " + PORT);
 });
 
 app.get("/", (req, res) => {
   res.send("hello");
+});
+
+app.post("/api/user", (req: Request<{}, {}, UserCreate>, res: Response) => {
+  const username = req.body.username;
+  res.json(username);
 });
