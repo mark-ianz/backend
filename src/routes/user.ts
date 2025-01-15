4de7a8bb336aee0082527d5c70b381f7bfc52d23
mark-ianz <bustillomarkian23@gmail.com>
2025-01-15 22:59:05 +0800

    added get router to get account info

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { createUser } from "../controllers/user";
+import { createUser, getAccountInfo } from "../controllers/user";
 
 const router = Router();
 
+router.get ("/:username", getAccountInfo)
 router.post("/", createUser);
 
 export default router;
